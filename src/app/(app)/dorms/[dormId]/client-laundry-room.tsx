eedfadf6f9b491ec73516dc867741309afbc71f0
zacheri04 <chalegoy5@gmail.com>
2026-02-25 21:37:41 -0600

    fixed ur dumbahh website with 1 line change

--- src/app/(app)/dorms/[dormId]/client-laundry-room.tsx
+++ src/app/(app)/dorms/[dormId]/client-laundry-room.tsx
@@ -81,68 +81,70 @@ export default function ClientLaundryRoom({
         {location.floor.displayName}
       </Link>
       <div className="flex flex-wrap gap-4">
-        {(machines ?? []).map((machine) => {
-          return (
-            <Card key={machine.identifier} className="w-80">
-              <CardHeader>
-                <CardTitle>
-                  <div className="flex flex-row items-center justify-between">
-                    <span>
-                      {machine.type.slice(0, 1).toUpperCase() +
-                        machine.type.slice(1)}
-                    </span>
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <button
-                          onClick={() => {
-                            void navigator.clipboard.writeText(
-                              machine.licensePlate,
-                            );
-                            setCopiedPlate(machine.licensePlate);
-                          }}
-                          className="bg-accent flex items-center gap-1 rounded-sm px-2 py-1 text-xs"
-                        >
-                          {copiedPlate === machine.licensePlate ? (
-                            <Check className="text-accent-foreground size-3" />
-                          ) : (
-                            <Clipboard className="text-accent-foreground size-3" />
-                          )}
-                          <p className="font-light">{machine.licensePlate}</p>
-                        </button>
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>
-                          {copiedPlate === machine.licensePlate
-                            ? "Copied!"
-                            : "Copy to clipboard"}
-                        </p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </div>
-                </CardTitle>
-              </CardHeader>
-              {machine.status === "in-use" && (
-                <CardContent>
-                  <div className="flex flex-col items-center justify-center gap-1">
-                    <p className="text-sm">
-                      {machine.timeRemaining === 1
-                        ? `${machine.timeRemaining} minute`
-                        : `${machine.timeRemaining} minutes`}
-                    </p>
-                    <Progress
-                      className="w-full"
-                      value={
-                        ((machine.defaultTotalTime - machine.timeRemaining) /
-                          machine.defaultTotalTime) *
-                        100
-                      }
-                    />
-                  </div>
-                </CardContent>
-              )}
-            </Card>
-          );
-        })}
+        {(machines ?? [])
+          .sort((a, b) => (a.type > b.type ? 1 : -1))
+          .map((machine) => {
+            return (
+              <Card key={machine.identifier} className="w-80">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex flex-row items-center justify-between">
+                      <span>
+                        {machine.type.slice(0, 1).toUpperCase() +
+                          machine.type.slice(1)}
+                      </span>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <button
+                            onClick={() => {
+                              void navigator.clipboard.writeText(
+                                machine.licensePlate,
+                              );
+                              setCopiedPlate(machine.licensePlate);
+                            }}
+                            className="bg-accent flex items-center gap-1 rounded-sm px-2 py-1 text-xs"
+                          >
+                            {copiedPlate === machine.licensePlate ? (
+                              <Check className="text-accent-foreground size-3" />
+                            ) : (
+                              <Clipboard className="text-accent-foreground size-3" />
+                            )}
+                            <p className="font-light">{machine.licensePlate}</p>
+                          </button>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>
+                            {copiedPlate === machine.licensePlate
+                              ? "Copied!"
+                              : "Copy to clipboard"}
+                          </p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                {machine.status === "in-use" && (
+                  <CardContent>
+                    <div className="flex flex-col items-center justify-center gap-1">
+                      <p className="text-sm">
+                        {machine.timeRemaining === 1
+                          ? `${machine.timeRemaining} minute`
+                          : `${machine.timeRemaining} minutes`}
+                      </p>
+                      <Progress
+                        className="w-full"
+                        value={
+                          ((machine.defaultTotalTime - machine.timeRemaining) /
+                            machine.defaultTotalTime) *
+                          100
+                        }
+                      />
+                    </div>
+                  </CardContent>
+                )}
+              </Card>
+            );
+          })}
       </div>
     </section>
   );
